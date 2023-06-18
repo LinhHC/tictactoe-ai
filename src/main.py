e424c9c4482dcda2c6e704e0253ffe5709f12dea
LinhHC <lhoangcong@hotmail.com>
2023-06-18 22:48:45 +0200

    added game board

--- src/main.py
+++ src/main.py
@@ -25,4 +25,17 @@ while go:
 
     screen.fill(black)
 
+    # draw board
+    board_size = 360
+    tile_size = board_size / 3
+    tiles = []
+    for i in range(3):
+        row = []
+        for j in range(3):
+            tile = pygame.Rect((screen_width - board_size) / 2 + i * tile_size,
+                               (screen_height - board_size) / 2 + j * tile_size, tile_size, tile_size)
+            pygame.draw.rect(screen, white, tile, 3)
+            row.append(tile)
+        tiles.append(row)
+
     pygame.display.flip()
