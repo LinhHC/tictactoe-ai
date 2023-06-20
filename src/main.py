cd029dcb48d197b9eef1e2b6362239da0e198449
LinhHC <lhoangcong@hotmail.com>
2023-06-20 23:16:05 +0200

    added game text

--- src/main.py
+++ src/main.py
@@ -10,9 +10,10 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Tic Tac Toe")
 
-# font
+# fonts
 retry_font = pygame.font.Font('font/OpenSans-Regular.ttf', 30)
-game_font = pygame.font.Font('font/OpenSans-Regular.ttf', 60)
+move_font = pygame.font.Font('font/OpenSans-Regular.ttf', 60)
+game_font = pygame.font.Font('font/OpenSans-Regular.ttf', 40)
 
 # colors
 black = (0, 0, 0)
@@ -25,7 +26,7 @@ board = ttt.initial_board()
 
 def draw(d_board, d_tile):
     if d_board != ttt.EMPTY:
-        move = game_font.render(d_board, True, white)
+        move = move_font.render(d_board, True, white)
         moveRect = move.get_rect()
         moveRect.center = d_tile.center
         screen.blit(move, moveRect)
@@ -57,10 +58,16 @@ while go:
             row.append(tile)
         tiles.append(row)
 
+    player = ttt.player(board)
     game_over = ttt.game_over(board)
 
+    if not game_over:
+        player_text = game_font.render(f"Turn: Player {player}", True, white)
+        playerRect = player_text.get_rect()
+        playerRect.center = ((screen_width / 2), (tile_size / 2))
+        screen.blit(player_text, playerRect)
+
     # players move
-    player = ttt.player(board)
     pressed, _, _ = pygame.mouse.get_pressed()
     if pressed and not game_over:
         pos = pygame.mouse.get_pos()
@@ -77,10 +84,20 @@ while go:
         retryButton = pygame.Rect(screen_width / 3, tile_size * 4 + (tile_size - 50) / 2, screen_width / 3, 50)
         pygame.draw.rect(screen, white, retryButton)
 
+        winner = ttt.winner(board)
+
         retry = retry_font.render("Retry", True, black)
         retryRect = retry.get_rect()
         retryRect.center = retryButton.center
         screen.blit(retry, retryRect)
+
+        if winner is not None:
+            gameover_text = game_font.render(f"Game Over: Player {winner} wins", True, white)
+        else:
+            gameover_text = game_font.render(f"Game Over: Tie", True, white)
+        gameoverRect = gameover_text.get_rect()
+        gameoverRect.center = ((screen_width / 2), (tile_size / 2))
+        screen.blit(gameover_text, gameoverRect)
 
         pressed, _, _ = pygame.mouse.get_pressed()
         if pressed:
